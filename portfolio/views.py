4a674c0094b09831379d60ddd2bbd97c5a7e3092
haiduk <hwlee91@gmail.com>
2021-02-28 03:19:43 -0500

    added portfolio detail view/urls

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+
+  return render(request, 'index.html')
+
+class PortfolioDetailView(DetailView):
+    model = Portfolio
+    template_name = "portfolio_detail.html"
+
+    
