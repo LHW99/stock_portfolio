c2ca2a4e03705c971248e5142bf390bce2c5f9c5
haiduk <hwlee91@gmail.com>
2021-03-01 03:24:13 -0500

    building class-based search view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,6 @@ from portfolio.views import *
 
 urlpatterns = [
   path('', IndexView.as_view(), name='index'),
-  path('portfolio', PortfolioDetailView.as_view(), name='portfolio_detail' )
+  path('portfolio', PortfolioDetailView.as_view(), name='portfolio_detail' ),
+  path('search', SearchView.as_view(), name='search'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,26 @@
 from portfolio.models import Stock, Portfolio
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
+from stock_portfolio.settings.private_settings import CLOUD_API_KEY
 
 class IndexView(TemplateView):
   template_name = 'index.html'
+
+class SearchView(DetailView):
+  model = Stock
+  template_name = 'search.html'
+
+  def get(self, request, **args, **kwargs):
+    query = request.GET('ticker_search')
+    symbol = query.upper()
+f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote,stats,advanced-stats&token={CLOUD_API_KEY}'
+
+  def get_context_data(self, *args, **kwargs):
+    context = super(SearchView, self).get_context 
+
+    if query: 
+
 
 class PortfolioDetailView(DetailView):
   model = Portfolio
