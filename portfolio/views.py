0143949dc3a265a44962de87dbb0f1ec10cef193
haiduk <hwlee91@gmail.com>
2021-03-02 22:08:34 -0500

    searchmixin and get_context_data

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,21 +7,27 @@ from stock_portfolio.settings.private_settings import CLOUD_API_KEY
 class IndexView(TemplateView):
   template_name = 'index.html'
 
-class SearchView(DetailView):
-  model = Stock
+class SearchMixin:
+  def get_ticker(self):
+    return self.request.GET.get('ticker_search')
+
+
+
+      query = request.GET['ticker_search']
+      symbol = query.upper()
+      response = request.get(f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote&token={CLOUD_API_KEY}')
+      data = response.json()
+
+
+class SearchView(SearchMixin, TemplateView):
   template_name = 'search.html'
 
-  def get(self, request, *args, **kwargs):
-    query = request.GET['ticker_search']
-    symbol = query.upper()
-    response = request.get(f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote&token={CLOUD_API_KEY}')
-    data = response.json()
-
-    return render(request, 'search.html', {
-      'symbol': symbol,
-      'companyName': data[symbol][quote][companyName],
-      'iexRealtimePrice': data[symbol][quote][iexRealtimePrice],
-    })
+  def get_content_data(self, **kwargs):
+    context = super(SearchView, self).get_context_data(**kwargs)
+    context['companyName'] = self.get_ticker(query_string)
+    context['iexRealtimePrice'] = self.get_ticker(query_string)
+    context['symbol'] = symbol
+    return context
 
 class PortfolioDetailView(DetailView):
   model = Portfolio
